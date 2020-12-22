30d5a6be53c925b768c541a3b85f1bd2791cb292
gilprad <gilangpraditto@gmail.com>
2020-12-22 08:22:58 +0700

    DONE

--- resources/views/component/edit.blade.php
+++ resources/views/component/edit.blade.php
@@ -37,7 +37,7 @@
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls">
                             <label>Content</label>
-                            <textarea rows="5" class="form-control" placeholder="Content" name="content" required data-validation-required-message="Please enter a content.">{{ old('content', $content->title) }}</textarea>
+                            <textarea rows="5" class="form-control" placeholder="Content" name="content" required data-validation-required-message="Please enter a content.">{{ old('content', $content->content) }}</textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
